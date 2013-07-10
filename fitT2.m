7d04e2ec31d3284fdfa592313b4c2519b7181a9f
Samuel Barnes <srbarnes@caltech.edu>
2013-07-09 18:39:40 -0700

    T1 FA fitting now tested and working Added linear T1 FA fitting

--- fitT2.m
+++ fitT2.m
@@ -23,8 +23,8 @@ if(strcmp(fit_type,'t1_tr_fit'))
 		r_squared = 0;
 	end
 elseif(strcmp(fit_type,'t1_fa_fit'))
-	y_lin = si./sin(te);
-	x_lin = si./tan(te);
+	y_lin = si./sin(pi/180*te);
+	x_lin = si./tan(pi/180*te);
 	Ybar = mean(y_lin(ok_)); 
 	Xbar = mean(x_lin(ok_));
 	y = y_lin(ok_)-Ybar;
@@ -35,6 +35,9 @@ elseif(strcmp(fit_type,'t1_fa_fit'))
 	if ~isfinite(r_squared)
 		r_squared = 0;
 	end
+elseif(strcmp(fit_type,'linear_fast') || strcmp(fit_type,'t1_fa_linear_fit'))
+	% Skip check as we are doing a fast linear fit
+	r_squared = 2.0;
 else
 	ln_si = log(si);
 	Ybar = mean(ln_si(ok_)); 
@@ -115,7 +118,9 @@ if r_squared>=0.7
 		slope =sum(x.*y)/sum(x.^2);
 		intercept = Ybar-slope.*Xbar; %#ok<NASGU>
 		r_squared = (sum(x.*y)/sqrt(sum(x.^2)*sum(y.^2)))^2;
-
+		if ~isfinite(r_squared)
+			r_squared = 0;
+		end
 		% Save Results
 		t2_fit = -1/slope;
 		rho_fit = intercept;
@@ -141,19 +146,24 @@ if r_squared>=0.7
 		t2_fit   = cf_.b;
 		t2_95_ci = confidence_interval(:,2);
 	elseif(strcmp(fit_type,'t1_fa_fit'))
+		% Convert flip angle (stored in te) from degrees to radians
+ 		te = te*pi/180;
+		% scale si, non-linear fit has trouble converging with big numbers
+		si = si./max(si);
+		
 		% Restrict fits for T1 from 0ms to 5000ms, and coefficient ('rho') from 
 		% 0 to inf
 		fo_ = fitoptions('method','NonlinearLeastSquares','Lower',[0 0],'Upper',[Inf   5000]);
-		st_ = [max(si) 500 ];
-		set(fo_,'Startpoint',st_);
+		st_ = [max(si)*10 500 ];
+ 		set(fo_,'Startpoint',st_);
 		%set(fo_,'Weight',w);
 		ft_ =  fittype('a*( (1-exp(-tr/t1))*sin(theta) )/( 1-exp(-tr/t1)*cos(theta) )',...
 			'dependent',{'si'},'independent',{'theta','tr'},...
 			'coefficients',{'a','t1'});
-
+		
 		% Fit the model
 		tr = [8 8 8 8 8 8];
-		[cf_, gof] = fit([te(ok_)',tr'],si(ok_)',ft_,fo_);
+		[cf_, gof] = fit([te(ok_),tr'],si(ok_),ft_,fo_);
 
 		% Save Results
 		r_squared = gof.rsquare;
@@ -161,7 +171,32 @@ if r_squared>=0.7
 		rho_fit = cf_.a;
 		t2_fit   = cf_.t1;
 		t2_95_ci = confidence_interval(:,2);
-
+	elseif(strcmp(fit_type,'t1_fa_linear_fit'))
+		y_lin = si./sin(pi/180*te);
+		x_lin = si./tan(pi/180*te);
+		
+		Ybar = mean(y_lin(ok_)); 
+		Xbar = mean(x_lin(ok_));
+		y = y_lin(ok_)-Ybar;
+		x = x_lin(ok_)-Xbar;
+		
+		slope =sum(x.*y)/sum(x.^2);
+		intercept = Ybar-slope.*Xbar; 
+		
+		r_squared = (sum(x.*y)/sqrt(sum(x.^2)*sum(y.^2)))^2;
+		if ~isfinite(r_squared)
+			r_squared = 0;
+		end
+		rho_fit = intercept;
+		t2_fit   = -8/log(slope);
+		if t2_fit>5000
+			t2_fit = 5001;
+		end
+		if t2_fit<0
+			t2_fit = -0.5;
+		end
+		t2_95_ci = [-1 -1];
+		
 	elseif(strcmp(fit_type,'none'))
 		r_squared = 1;
 		rho_fit = 1;
